dfac41861e3d6dd89793e9307defd7e5f30f0e07
Shashwat Saxena <shashwat.saxena612@gmail.com>
2024-05-19 22:28:15 +0530

    found what i was looking for

--- extension/scripts/background.js
+++ extension/scripts/background.js
@@ -1,28 +1,51 @@
 let cnt = 0;
-
+let timeForURL = {};
+let mappingURL = {};
+let timeForTab = {};
+let time = new Date().getTime();
+let currentActive = -1;
 
 chrome.tabs.onCreated.addListener(function(t){
-  console.log(t);
-  if(t.active){
-    console.log(cnt++);
-  }
+  // console.log(t);
+  // if(t.active){
+  //   console.log(cnt++);
+  // }
 });
 
+chrome.tabs.onHighlighted.addListener(function(t, wid){
+  console.log(t, wid)
+})
 
-chrome.tabs.onActivated.addListener(function (t) {
+
+chrome.tabs.onActivated.addListener(async function (t) {
   console.log("activated", t)
+  const x= await chrome.tabs.query({  active: true, lastFocusedWindow: true })
+  console.log(x)
 })
 
 
 
 chrome.tabs.onUpdated.addListener(function(t , tab){
-  console.log("updated", t, tab);
+  // console.log("updated", t, tab);
+  if(tab.url){
+    const url = tab.url;
+    const siteName = url.match(/:\/\/(.[^/]+)/)[1];
+    console.log(siteName);
+
+  }
+  // console.log(t, (new Date().getTime() - time) / 60000)
+  // time = new Date().getTime();
+
 });
 
+chrome.windows.onFocusChanged.addListener(function(windowId) {
+  console.log(windowId)
+});
 
-
-chrome.tabs.onRemoved.addListener(function(t, tabInfo){
+chrome.tabs.onRemoved.addListener(async function(t, tabInfo){
   console.log("closed", t, tabInfo);
+  const x = await chrome.tabs.query({});
+  console.log(x)
 });
 
 
@@ -36,12 +59,14 @@ chrome.action.onClicked.addListener(async (tab) => {
         tabId: tab.id,
         text: nextState,
     });
+
     if (nextState === "ON") {
       await chrome.scripting.insertCSS({
         files: ["/extensions/background.css"],
         target: { tabId: tab.id },
       });
-    } else if (nextState === "OFF") {
+    } 
+    else if (nextState === "OFF") {
       await chrome.scripting.removeCSS({
         files: ["/extensions/background.css"],
         target: { tabId: tab.id },
--- extension/scripts/contentScript.js
+++ extension/scripts/contentScript.js
@@ -2,3 +2,5 @@ console.log("Hello from content script!");
 
 console.log(document.location.href);
 
+if(document)
+    console.log(document.querySelectorAll("hi"))
